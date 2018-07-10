73d9fea3833d8f6daec6033fc9e332be4e48f9f5
ThauNguyen <hoangthau627@gmail.com>
2018-07-10 20:43:14 +0700

    create action

--- src/containers/BookList.js
+++ src/containers/BookList.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { selectBook } from '../actions/index';
 
 class BookList extends Component {
   renderList() {
-    this.props.books.map(book => {
+    return this.props.books.map(book => {
       return (
         <li key={book.title} className="list-group-item">
           {book.title}
@@ -23,4 +25,11 @@ function mapStateToProps(state) {
   };
 }
 
-export default connect(mapStateToProps)(BookList);
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ selectBook: selectBook }, dispatch);
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(BookList);
